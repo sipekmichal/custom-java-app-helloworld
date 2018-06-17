6c880f581fcd09dfd9f2f76a6d9af27ae91e75bf
Michal Sipek <michal.sipek@aevi.cz>
2018-06-17 11:08:35 +0200

    Test scanner

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -6,10 +6,19 @@ public class Main
     {
         System.out.println("Hello world!");
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your nationality: ");
-        String nationality = scanner.nextLine();
-
-        System.out.print("Enter your age: ");
-        int age = scanner.nextInt();
+        try {
+            while (true) {
+                System.out.println("Please input a line");
+                long then = System.currentTimeMillis();
+                String line = scanner.nextLine();
+                long now = System.currentTimeMillis();
+                System.out.printf("Waited %.3fs for user input%n", (now - then) / 1000d);
+                System.out.printf("User input was: %s%n", line);
+            }
+        } catch(IllegalStateException | NoSuchElementException e) {
+            // System.in has been closed
+            System.out.println("System.in was closed; exiting");
+        }
+    }
     }
 }
