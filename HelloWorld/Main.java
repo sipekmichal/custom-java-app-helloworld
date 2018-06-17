5c6df5bcc8f12301fca2b418824709a4675798e2
Michal Sipek <michal.sipek@aevi.cz>
2018-06-17 11:27:45 +0200

    Removed printl

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -5,11 +5,11 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
-        System.out.println("Press \"ENTER\" to exit this program...");
         try {
+            // waiting for enter to exit this program
             int read = System.in.read(new byte[2]);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    }
+}
